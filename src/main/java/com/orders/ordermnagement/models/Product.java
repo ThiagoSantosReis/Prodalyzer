b3e5a91243825086ab8cbd03a0efda1da1fd7502
thiago.san <thisantosareis@gmail.com>
2025-07-03 16:51:35 -0300

    first commit

---
+++ src/main/java/com/orders/ordermnagement/models/Product.java
@@ -0,0 +1,8 @@
+package com.orders.ordermnagement.models;
+
+public class Product {
+    private Long id;
+    private String title;
+    private Double price;
+    private Category category;
+}
---
+++ src/test/java/com/orders/ordermnagement/OrdermnagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orders.ordermnagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermnagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

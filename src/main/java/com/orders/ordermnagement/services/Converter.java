fbac981f37a595f18662493713d8653b85462057
thiago.san <thisantosareis@gmail.com>
2025-07-03 18:03:40 -0300

    implement class for json deserialization

--- src/main/java/com/orders/ordermnagement/services/Converter.java
+++ src/main/java/com/orders/ordermnagement/services/Converter.java
@@ -1,4 +1,20 @@
 package com.orders.ordermnagement.services;
 
-public class Converter {
+import aj.org.objectweb.asm.TypeReference;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Converter implements IConverter{
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    public <T> T getData(String json, Class<T> cls) {
+        try {
+            return mapper.readValue(json, cls);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/main/java/com/orders/ordermnagement/services/IConverter.java
+++ src/main/java/com/orders/ordermnagement/services/IConverter.java
@@ -1,5 +1,7 @@
 package com.orders.ordermnagement.services;
 
+import aj.org.objectweb.asm.TypeReference;
+
 public interface IConverter {
     <T> T getData(String json, Class<T> cls);
 }
